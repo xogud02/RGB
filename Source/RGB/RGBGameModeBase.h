421f455b3d596611e0a1de9b6b98c451f4e713d7
xogud02 <xogud02@naver.com>
2019-07-29 12:55:57 +0900

    1st commit

---
+++ Source/RGB/RGBGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RGBGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGB_API ARGBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
